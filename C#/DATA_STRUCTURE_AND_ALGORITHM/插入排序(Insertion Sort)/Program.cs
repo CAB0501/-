d322d0c26018749137de7f39b55318d2c2db5be9
zhegemao <779145208@qq.com>
2020-05-27 12:24:49 +0800

    插入排序

---
+++ C#/DATA_STRUCTURE_AND_ALGORITHM/插入排序(Insertion Sort)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace 插入排序_Insertion_Sort_
+{
+	internal class Program
+	{
+		private static void InsertionSort(int[] arr)
+		{
+			if (arr is null)
+			{
+				return;
+			}
+			int left = 0;
+			while (left < arr.Length - 1)
+			{
+				int i = ++left;
+				while (i > 0)
+				{
+					if (arr[i] < arr[i - 1])
+					{
+						int temp = arr[i];
+						arr[i] = arr[i - 1];
+						arr[--i] = temp;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+		}
+
+		private static void Main(string[] args)
+		{
+			int[] nums = { 5, 1, 34, 12, 33, 42, 83, 2, 0, 4, 10, 6, 13 };
+			InsertionSort(nums);
+			string sortnums = "";
+			foreach (int num in nums)
+			{
+				sortnums += $"{num},";
+			}
+			Console.WriteLine(sortnums[0..^1]);
+		}
+	}
+}
